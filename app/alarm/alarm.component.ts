5dd649b16802b3c1af08aaa12e14cdc04428b555
Mahendra Tennakoon <mahendrathennakoon@gmail.com>
2017-12-21 05:50:35 +0530

    Create AlarmComponent

---
+++ app/alarm/alarm.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "ns-alarms",
+    moduleId: module.id,
+    templateUrl: "./alarm.component.html",
+})
+export class AlarmComponent implements OnInit {
+
+    constructor(
+    ) { }
+
+    ngOnInit(): void {
+        
+    }
+}
--- app/app.routing.ts
+++ app/app.routing.ts
@@ -5,10 +5,13 @@ import { Routes } from "@angular/router";
 import { ItemsComponent } from "./item/items.component";
 import { ItemDetailComponent } from "./item/item-detail.component";
 
+import { AlarmComponent } from "./alarm/alarm.component";
+
 const routes: Routes = [
-    { path: "", redirectTo: "/items", pathMatch: "full" },
+    { path: "", redirectTo: "/alarms", pathMatch: "full" },
     { path: "items", component: ItemsComponent },
     { path: "item/:id", component: ItemDetailComponent },
+    { path: "alarms", component: AlarmComponent }
 ];
 
 @NgModule({
